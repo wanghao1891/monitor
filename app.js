3d15cca4b15c58702ff9cb810129e3798f8232f3
Wanghao <wanghao1891@126.com>
2015-03-27 22:05:52 +0800

    express deprecated res.sendfile: Use res.sendFile instead.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var io = require('socket.io')(server);
 server.listen(80);
 
 app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
